488515232d79a55a471cbeb14301e67af97c1004
Noora78455 <62265214+Noora78455@users.noreply.github.com>
2022-07-20 10:52:58 +0100

    Update config.php

--- database/config.php
+++ database/config.php
@@ -15,19 +15,3 @@ $username   = 'nourrad';
 $password   = 'password';
 $db_name     = 'sampledb';
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
-// etablissement de la connection 
-
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, $sslcert, NULL, NULL);
-mysqli_real_connect($conn, $host, $username, $password,
- $db_name, 3306, MYSQLI_CLIENT_SSL);
-//if (mysqli_connect_errno($conn){
-  //  die('failed to connect to MysqL: '.mysqli_connect_erro());
-//}
-// run la table produit
-if (mysqli_query($conn,'CREATE TABLE Products (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- ProductName VARCHAR(50) NOT NULL, 
-Price DOUBLE NOT NULL );' )){
-printf("table created\n");
-}
